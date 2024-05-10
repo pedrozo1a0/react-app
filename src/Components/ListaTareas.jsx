6acc0a874c491ee07f737b5b78e35fdc169ad4b7
pedrozo1a0 <ale.pedrozo.952@gmail.com>
2024-05-10 14:35:21 -0400

    first commit

---
+++ src/Components/ListaTareas.jsx
@@ -0,0 +1,13 @@
+export function ListaTareas(){
+    return(
+        <section>
+            <p>Things I need to do: </p>
+        <ul>
+            <li>Learn React</li>
+            <li>Climb Mt. Everest</li>
+            <li>Run a Marathon</li>
+            <li>Feed the dogs</li>
+        </ul>
+        </section>
+    )
+}
